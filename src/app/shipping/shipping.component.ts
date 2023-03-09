c81318dab25174ed652c0825050df95bb7e1ed16
torokt21 <torokt21@gmail.com>
2023-03-09 12:14:05 +0100

    Add shipping prices

---
+++ src/app/shipping/shipping.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { CartService } from '../cart.service';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-shipping',
+  templateUrl: './shipping.component.html',
+  styleUrls: ['./shipping.component.css'],
+})
+export class ShippingComponent {
+  shippingCosts!: Observable<{ type: string; price: number }[]>;
+
+  constructor(private cartService: CartService) {}
+
+  ngOnInit() {
+    this.shippingCosts = this.cartService.getShippingPrices();
+  }
+}
